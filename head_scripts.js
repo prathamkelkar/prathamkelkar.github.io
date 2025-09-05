6723cb2c138a0530b69667658276e754d622108b
prathamkelkar <133006257+prathamkelkar@users.noreply.github.com>
2025-09-05 10:22:17 +0530

    Update head_scripts.js

--- head_scripts.js
+++ head_scripts.js
@@ -13,3 +13,10 @@ tailwind.config = {
         }
     }
 }
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XXXXXXXXXX');
+
